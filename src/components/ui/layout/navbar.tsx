89375ae62adf0068efe24fe0f7de28146373ccb3
shafi2t5 <shafiul.islam@brainstation-23.com>
2022-12-16 23:56:41 +0600

    layout setup

--- src/components/ui/layout/navbar.tsx
+++ src/components/ui/layout/navbar.tsx
@@ -1,24 +1,26 @@
 import React from "react";
-import type { MenuProps } from "antd";
+// import type { MenuProps } from "antd";
 import { Layout, Menu } from "antd";
 
 const { Header } = Layout;
 
-const items1: MenuProps["items"] = ["1", "2", "3"].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
+// const items1: MenuProps["items"] = ["1", "2", "3"].map((key) => ({
+//   key,
+//   label: `nav ${key}`,
+// }));
 
 const Navbar: React.FC = () => {
   return (
     <Header className="header">
-      <div className="logo" title="Admin Panel" />
-      <Menu
+      <div className="logo" title="Admin Panel">
+        Admin Panel
+      </div>
+      {/* <Menu
         theme="dark"
         mode="horizontal"
         defaultSelectedKeys={["2"]}
         items={items1}
-      />
+      /> */}
     </Header>
   );
 };
